0f00a323c24b8bc35bc6bae95b75e2f7f8ca89d6
DenisKutuzov <Denis-kutuzofff@yandex.ru>
2023-04-12 23:31:11 +0600

    t

---
+++ src/components/Header/DropDown/DropDown.tsx
@@ -0,0 +1,13 @@
+import style from './DropDown.module.scss'
+
+const DropDown = () => {
+  return (
+    <>
+    <div className={style.dropdown}>
+
+    </div>
+    </>
+   )
+}
+
+export default DropDown
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,73 +1,99 @@
 import { useState } from 'react'
 import style from './header.module.scss'
 import Image from 'next/image'
-
+import Link from 'next/link'
+import DropDown from './DropDown/DropDown'
 
 const Header = () => {
   const [headerModul, setHeaderModul] = useState(false)
 
+  const handleMouseEnter = () => {
+    setHeaderModul(!headerModul)
+  }
+
+  const handleMouseLeave = () => {
+    setHeaderModul(!headerModul)
+  }
+
+  console.log(headerModul)
 
   return (
     <div className={style.wrapper}>
       <div className={style.header}>
         <div className={style.header__logo}>
-          <a href='#!' title='Онлайн-кинотеатр Иви'>
+          <Link href='#!' title='Онлайн-кинотеатр Иви'>
             <Image
               src='/img/header-logo.svg'
               width={66}
               height={48}
               alt='ivi'
             />
-          </a>
+          </Link>
         </div>
-        <div className={style.DIV_1}>
-          <nav className={style.NAV_2}>
-            <ul className={style.UL_3}>
-              <li className={style.LI_4}>
-                <a href='https://www.ivi.ru/' title='Мой Иви' className={style.A_5}>Мой Иви</a>
-                <div className={style.DIV_6} onClick={() => setHeaderModul(!headerModul)}>123</div>
-                {headerModul && <div>12334254535</div>}
+      
+          <nav className={style.navbar}>
+            <ul className={style.navbar__ul}>
+              <li className={style.navbar__li}>
+                <Link
+                  href='https://www.ivi.ru/'
+                  title='Мой Иви'
+                  className={style.navbar__a}
+                >
+                  Мой Иви
+                </Link>
+          
+             
               </li>
-              <li className={style.LI_4}>
-                <a
+              <li className={style.navbar__li}>
+                <Link
                   href='https://www.ivi.ru/new'
                   title='Что нового'
-                  className={style.A_5}
-                ></a>
-                <div className={style.DIV_6}>Что нового</div>
+                  className={style.navbar__a}
+                >Что нового</Link>
               </li>
-              <li className={style.LI_4}>
-                <a
+              <li className={style.navbar__li}  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}>
+                <Link
                   href='https://www.ivi.ru/movies'
                   title='Фильмы онлайн'
-                  className={style.A_5}
-                ></a>
-                <div className={style.DIV_6}>Фильмы</div>
+                  className={style.navbar__a}
+                >Фильмы</Link>
               </li>
-              <li className={style.LI_4}>
-                <a
+              <li className={style.navbar__li}  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}>
+                <Link
                   href='https://www.ivi.ru/series'
                   title='Сериалы онлайн'
-                  className={style.A_5}
-                ></a>
-                <div className={style.DIV_6}>Сериалы</div>
+                  className={style.navbar__a}
+                >Сериалы</Link>
               </li>
-              <li className={style.LI_4}>
-                <a
+              <li className={style.navbar__li}  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}>
+                <Link
                   href='https://www.ivi.ru/animation'
                   title='Мультфильмы онлайн'
-                  className={style.A_5}
-                ></a>
-                <div className={style.DIV_6}>Мультфильмы</div>
+                  className={style.navbar__a}
+                >Мультфильмы</Link>
               </li>
-              <li className={style.LI_4}>
-                <a href='https://www.ivi.ru/tvplus' title='TV+' className={style.A_5}></a>
-                <div className={style.DIV_6}>TV+</div>
+              <li className={style.navbar__li}  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}>
+                <Link
+                  href='https://www.ivi.ru/tvplus'
+                  title='TV+'
+                  className={style.navbar__a}
+                >TV+</Link>
               </li>
             </ul>
+            
           </nav>
+          {headerModul && <div>12334254535</div>}
+        <div className={style.header__right}>
+          <button className={style.DIV_1}>Оплатить подписку</button>
+          <button className={style.DIV_1}>Оплатить подписку</button>
         </div>
+        <DropDown/>
       </div>
+
     </div>
   )
 }
