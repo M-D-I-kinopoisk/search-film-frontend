b7917e9ef8a7a00f9404b55d9b542e2b5f0c3e64
Masthead <alexeev.tolik2015@yandex.ru>
2023-04-15 22:07:25 +0300

    deleted garbage

--- src/components/ActorFilmItem/ActorFilmItem.tsx
+++ src/components/ActorFilmItem/ActorFilmItem.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import style from './ActorFilmItem.module.scss'
 
-type ActorFilmItemProps = {
-}
+type ActorFilmItemProps = {}
 
 const ActorFilmItem: React.FC<ActorFilmItemProps> = () => {
     return (
         <div className={style.item}>
             <div>
                 <img
-                    src="https://thumbs.dfs.ivi.ru/storage38/contents/0/0/55c8a2551297804e709c65c9b4ecc0.jpg/172x264/?q=85" />
+                    src="https://thumbs.dfs.ivi.ru/storage38/contents/0/0/55c8a2551297804e709c65c9b4ecc0.jpg/172x264/?q=85"/>
             </div>
 
             <div className={style.info}>
