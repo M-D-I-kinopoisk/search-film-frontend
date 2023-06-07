bd84648492aa3302bbb2d6d89e46cb3d5aa4ee6a
DenisKutuzov <Denis-kutuzofff@yandex.ru>
2023-06-07 20:31:02 +0600

    not-found

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,12 +1,11 @@
-import Link from 'next/link'
 import styles from './not-found.module.scss'
 
 export default function NotFound() {
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
-            <div className={styles.error}>Ошибка</div>
-            <div className={styles.text}>Запрашиваемой страницы не существует</div>
+                <div className={styles.error}>Ошибка</div>
+                <div className={styles.text}>Запрашиваемой страницы не существует</div>
             </div>
         </div>
     )
